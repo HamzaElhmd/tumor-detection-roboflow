dddbc8903a7296531514b9448665109e9bddcdd3
LE Linh Long <lelinhlong@gmail.com>
2026-06-19 00:10:01 +0200

    Update

--- kaggle_support/get_data.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="vRFeOf1PlTttJjJ3YEvH")
-project = rf.workspace("kanye2028").project("brain-tumor-aqm9n-tgwbp")
-version = project.version(1)
-dataset = version.download("folder")
